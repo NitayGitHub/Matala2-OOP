798baa1ea13d50659937270727345dd999bb7273
Nitay <nitaylevi1998@gmail.com>
2023-01-01 22:41:56 +0200

    FunctionsNo3-01/01

--- Ex2.java
+++ Ex2.java
@@ -1,7 +1,8 @@
 public class Ex2 {
     public static void main(String[] args) {
-        String s[] = Ex2_1.createTextFiles(4, 1, 10);
-        int x = Ex2_1.getNumOfLines(s);
+        String s[] = Ex2_1.createTextFiles(4, 1, 9);
+        int x = Ex2_1.getNumOfLinesThreads(s);
         System.out.println(x);
+
     }
 }
--- Ex2_1.java
+++ Ex2_1.java
@@ -1,7 +1,10 @@
 import java.io.*;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.CountDownLatch;
 
 public class Ex2_1 {
+    private static AtomicInteger threadsCount = new AtomicInteger();
 
     public static String[] createTextFiles(int n, int seed, int bound) {
         File myDir = new File("C:\\temp\\Ex2");
@@ -30,26 +33,64 @@ public class Ex2_1 {
         return myDir.list();
     }
 
+    private static int countFileLines(String fileName) {
+        int count = 0;
+        try {
+            FileReader fr = new FileReader("C:\\temp\\Ex2\\" + fileName);
+            BufferedReader br = new BufferedReader(fr);
+            String str = br.readLine();
+            while (str != null) {
+                count++;
+                str = br.readLine();
+            }
+            br.close();
+            fr.close();
+        } catch (IOException ex) {
+            System.err.println("File read failed");
+            ex.printStackTrace();
+        }
+        return count;
+    }
+
+
     public static int getNumOfLines(String[] fileNames) {
         int count = 0;
         for (String fileName : fileNames) {
-            try {
-                FileReader fr = new FileReader("C:\\temp\\Ex2\\" + fileName);
-                BufferedReader br = new BufferedReader(fr);
-                String str = br.readLine();
-                while (str != null) {
-                    count++;
-                    str = br.readLine();
-                }
-                br.close();
-                fr.close();
-            } catch (IOException ex) {
-                System.err.println("File read failed");
-                ex.printStackTrace();
-            }
+            count += countFileLines(fileName);
+        }
+        return count;
+    }
 
+    public static class NOLThread extends Thread {
+        private final String fileName;
+        private final CountDownLatch latch;
+
+        public NOLThread(String fileName, CountDownLatch latch) {
+            this.fileName = fileName;
+            this.latch = latch;
         }
 
-        return count;
+        @Override
+        synchronized public void run() {
+            threadsCount.addAndGet(countFileLines(fileName));
+            latch.countDown();
+        }
+    }
+
+    public static int getNumOfLinesThreads(String[] fileNames) {
+        int res;
+        CountDownLatch latch = new CountDownLatch(fileNames.length);
+        for (String fileName : fileNames) {
+            new NOLThread(fileName, latch).start();
+        }
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        res = threadsCount.get();
+        threadsCount.set(0);
+        return res;
     }
 }
