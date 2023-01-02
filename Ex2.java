9f541a89179ffac4dd46c70e2114407abfad122d
Nitay <nitaylevi1998@gmail.com>
2023-01-02 13:41:56 +0200

    upd-02/01

--- Ex2.java
+++ Ex2.java
@@ -1,10 +1,19 @@
+
 public class Ex2 {
     public static void main(String[] args) {
-        String[] s = Ex2_1.createTextFiles(4, 1, 9);
-
-        int x = Ex2_1.getNumOfLinesThreadPool(s);
-        System.out.println(x);
-
+        String[] s = Ex2_1.createTextFiles(30, 1, 15);
+        long start1 = System.currentTimeMillis();
+        Ex2_1.getNumOfLines(s);
+        long end1 = System.currentTimeMillis();
+        System.out.println("Elapsed Time for function 2 in milliseconds: "+ (end1-start1));
+        long start2 = System.currentTimeMillis();
+        Ex2_1.getNumOfLinesThreads(s);
+        long end2 = System.currentTimeMillis();
+        System.out.println("Elapsed Time for function 3 in milliseconds: "+ (end2-start2));
+        long start3 = System.currentTimeMillis();
+        Ex2_1.getNumOfLinesThreadPool(s);
+        long end3 = System.currentTimeMillis();
+        System.out.println("Elapsed Time for function 4 in milliseconds: "+ (end3-start3));
 
     }
 }
