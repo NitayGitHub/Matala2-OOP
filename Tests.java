1116f2ac7852fc3f2baed573ba7dd081d041ef0b
naor554 <117055432+naor554@users.noreply.github.com>
2023-01-11 17:35:21 +0200

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,25 @@
+package Matala_2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Test_PartA {
+
+    @Test
+    void test(){
+        String[] s = Ex2_1.createTextFiles(500, 1, 100);
+        long start1 = System.currentTimeMillis();
+        int x = Ex2_1.getNumOfLines(s);
+        long end1 = System.currentTimeMillis();
+
+        long start2 = System.currentTimeMillis();
+        int y = Ex2_1.getNumOfLinesThreads(s);
+        long end2 = System.currentTimeMillis();
+
+        long start3 = System.currentTimeMillis();
+        int z = Ex2_1.getNumOfLinesThreadPool(s);
+        long end3 = System.currentTimeMillis();
+
+        assertTrue((end1 - start1) < (end2 - start2) &&  (end2 - start2) < (end3 - start3));
+    }
+}
